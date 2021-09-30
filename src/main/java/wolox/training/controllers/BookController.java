a88a6def30f4cf92224e5cf1b4e2eddee16f3654
juancruz.poli.contractor <juancruz.poli.contractor@gdevtools.com>
2021-09-30 13:34:17 -0300

    Exceptions - model - repository and javadocs added

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,7 +79,7 @@ public class BookController {
      *
      * @throws BookNotFoundException returns an exception if the book is not found
      */
-    @RequestMapping(value = "/{id}", method = DELETE)
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable Long id) {
         bookRepository.findById(id)
